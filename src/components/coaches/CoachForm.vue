9da0545f2de27b6ce332c7724b74f6d032e7f375
Matias Cabrejos <matias.cabrejos@hotmail.com>
2021-07-31 15:46:46 -0300

    Adding things to vuex

--- src/components/coaches/CoachForm.vue
+++ src/components/coaches/CoachForm.vue
@@ -1,64 +1,83 @@
 <template>
-    <form action="" @submit.prevent="submitForm">
-        <div class="form-control">
-            <label for="firstname">Firstname</label>
-            <input type="text" name="" id="firstname" v-model.trim="firstName">
-        </div>
-        <div class="form-control">
-            <label for="lastname">Lastname</label>
-            <input type="text" name="" id="lastname" v-model.trim="lastName">
-        </div>
-        <div class="form-control">
-            <label for="description">Description</label>
-            <textarea id="description" rows="5" v-model.trim="description"></textarea>
-        </div>
-        <div class="form-control">
-            <label for="rate">Hourly Rate</label>
-            <input type="number" name="" id="rate" v-model.number="rate">
-        </div>
-        <div class="form-control">
-            <h3>Areas of Expertise</h3>
-            <div>
-                <input type="checkbox" name="" id="frontend" value="frontend" v-model="areas">
-                <label for="frontend">Frontend Development</label>
-            </div>
-            <div>
-                <input type="checkbox" name="" id="backend" value="backend" v-model="areas">
-                <label for="backend">Backend Development</label>
-            </div>
-            <div>
-                <input type="checkbox" name="" id="career" value="career" v-model="areas">
-                <label for="career">Career Advisory</label>
-            </div>
-        </div>
-        <base-button>Register</base-button>
-    </form>
+  <form action="" @submit.prevent="submitForm">
+    <div class="form-control">
+      <label for="firstname">Firstname</label>
+      <input type="text" name="" id="firstname" v-model.trim="firstName" />
+    </div>
+    <div class="form-control">
+      <label for="lastname">Lastname</label>
+      <input type="text" name="" id="lastname" v-model.trim="lastName" />
+    </div>
+    <div class="form-control">
+      <label for="description">Description</label>
+      <textarea id="description" rows="5" v-model.trim="description"></textarea>
+    </div>
+    <div class="form-control">
+      <label for="rate">Hourly Rate</label>
+      <input type="number" name="" id="rate" v-model.number="rate" />
+    </div>
+    <div class="form-control">
+      <h3>Areas of Expertise</h3>
+      <div>
+        <input
+          type="checkbox"
+          name=""
+          id="frontend"
+          value="frontend"
+          v-model="areas"
+        />
+        <label for="frontend">Frontend Development</label>
+      </div>
+      <div>
+        <input
+          type="checkbox"
+          name=""
+          id="backend"
+          value="backend"
+          v-model="areas"
+        />
+        <label for="backend">Backend Development</label>
+      </div>
+      <div>
+        <input
+          type="checkbox"
+          name=""
+          id="career"
+          value="career"
+          v-model="areas"
+        />
+        <label for="career">Career Advisory</label>
+      </div>
+    </div>
+    <base-button>Register</base-button>
+  </form>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            firstName: '',
-            lastName: '',
-            description: '',
-            rate: null,
-            areas: []
-        }
-    },
-    methods: {
-        submitForm() {
-            const formData = {
-                first: this.firstName,
-                last: this.lastName,
-                desc: this.description,
-                rate: this.rate,
-                areas: this.areas
-            };
-
-            console.log(formData);
-        }
+  emits: ['save-data'],
+  data() {
+    return {
+      firstName: '',
+      lastName: '',
+      description: '',
+      rate: null,
+      areas: [],
     }
+  },
+  methods: {
+    submitForm() {
+      const formData = {
+        first: this.firstName,
+        last: this.lastName,
+        desc: this.description,
+        rate: this.rate,
+        areas: this.areas,
+      }
+
+      this.$emit('save-data', formData);
+    },
+  },
 }
 </script>
 
