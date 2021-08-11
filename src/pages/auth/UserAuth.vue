145343674e374abf5e2099751da9341105440e97
Matias Cabrejos <matias.cabrejos@hotmail.com>
2021-08-10 22:53:04 -0300

    Adding new vuex module

--- src/pages/auth/UserAuth.vue
+++ src/pages/auth/UserAuth.vue
@@ -1,19 +1,66 @@
 <template>
   <base-card>
-    <form action="">
+    <form @submit.prevent="submitForm">
         <div class="form-control">
             <label for="email">E-Mail</label>
-            <input type="email" id="email">
+            <input type="email" id="email" v-model.trim="email">
         </div>
         <div class="form-control">
             <label for="password">Password</label>
-            <input type="password" id="email">
+            <input type="password" id="email" v-model.trim="password">
         </div>
-        <base-button>Login</base-button>
-        <base-button type="button" mode="flat">Signup instead</base-button>
+        <p v-if="!formIsValid">Please enter a valid email and password (must be at least 6 characters long).</p>
+        <base-button>{{ submitButtonCaption }}</base-button>
+        <base-button type="button" mode="flat" @click="switchAuthMode">{{ switchModeButtonCaption }}</base-button>
     </form>
   </base-card>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+      formIsValid: true,
+      mode: 'login' 
+    };    
+  },
+  computed: {
+    submitButtonCaption() {
+      if (this.mode === 'login') {
+        return 'Login';
+      } else {
+        return 'Signup';
+      }
+    },
+    switchModeButtonCaption() {
+      if (this.mode === 'login') {
+        return 'Signup instead';
+      } else {
+        return 'Login instead';
+      }
+    },
+  },
+  methods: {
+    submitForm() {
+      this.formIsValid = true;
+      if (this.email === '' || !this.email.includes('@') || this.password.length < 6) {
+        this.formIsValid = false;
+        return 
+      }
+      // send http req...
+    },
+    switchAuthMode() {
+      if (this.mode === 'login') {
+        this.mode = 'signup';
+      } else {
+        this.mode = 'login';
+      }
+    }
+  }
+}
+</script>
 
 <style scoped>
 form {
