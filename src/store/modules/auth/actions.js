8c523f43a77a2c595732b16bc4653b54aa0758ec
Matias Cabrejos <matias.cabrejos@hotmail.com>
2021-08-20 21:56:38 -0300

    Adding auto login

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,32 +1,23 @@
 export  default {
     async login(context, payload) {
-        const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCHhqEiX6dxUNyXHsaYbRCQOgnSRhE4JmM', {
-            method: 'POST',
-            body: JSON.stringify({
-                email: payload.email,
-                password: payload.password,
-                returnSecureToken: true
-            })
-        });
-
-        const responseData = await response.json();
-
-        if(!response.ok) {
-            console.log(responseData);
-            const error = new Error(responseData.message || 'Failed to authenticate. Check your login data.');
-            throw error;
-        }
-
-        console.log(responseData);
-        context.commit('setUser', {
-            token: responseData.idToken,
-            userId: responseData.localId,
-            tokenExpiration: responseData.expiresIn 
-        });
-        
+        return context.dispatch('auth', {
+            ...payload,
+            mode: 'login'
+        });                
     },
     async signup(context, payload) {
-        const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCHhqEiX6dxUNyXHsaYbRCQOgnSRhE4JmM', {
+        return context.dispatch('auth', {
+            ...payload,
+            mode: 'signup'
+        });          
+    },
+    async auth(context, payload) {
+        const mode = payload.mode;
+        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCHhqEiX6dxUNyXHsaYbRCQOgnSRhE4JmM';
+        if (mode === 'signup') {
+            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCHhqEiX6dxUNyXHsaYbRCQOgnSRhE4JmM'
+        }
+        const response = await fetch(url, {
             method: 'POST',
             body: JSON.stringify({
                 email: payload.email,
@@ -34,21 +25,34 @@ export  default {
                 returnSecureToken: true
             })
         });
-        
+
         const responseData = await response.json();
 
         if(!response.ok) {
-            console.log(responseData);
             const error = new Error(responseData.message || 'Failed to authenticate. Check your login data.');
             throw error;
         }
 
-        console.log(responseData);
+        localStorage.setItem('token', responseData.idToken);
+        localStorage.setItem('userId', responseData.localId);
+
         context.commit('setUser', {
             token: responseData.idToken,
             userId: responseData.localId,
             tokenExpiration: responseData.expiresIn 
         });
+    },
+    tryLogin(context) {
+        const token = localStorage.getItem('token');
+        const userId = localStorage.getItem('userId');
+        
+        if (token && userId) {
+            context.commit('setUser', {
+                token: token,
+                userId: userId,
+                tokenExpiration: null
+            });
+        }
     },
     logout(context) {
         context.commit('setUser', {
